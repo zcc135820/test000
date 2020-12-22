148d914120faa48bbbe3b2843f14a0d72f0ec891
xiaofei-ya <75562054+xiaofei-ya@users.noreply.github.com>
2020-12-22 19:01:27 +0800

    Update web_fuzz_sub.go

--- pkg/getter/web_fuzz_sub.go
+++ pkg/getter/web_fuzz_sub.go
@@ -1,13 +1,13 @@
 package getter
 
 import (
-	"github.com/Sansui233/proxypool/log"
+	"github.com/xiaofei-ya/proxypool/log"
 	"io/ioutil"
 	"regexp"
 	"sync"
 
-	"github.com/Sansui233/proxypool/pkg/proxy"
-	"github.com/Sansui233/proxypool/pkg/tool"
+	"github.com/xiaofei-ya/proxypool/pkg/proxy"
+	"github.com/xiaofei-ya/proxypool/pkg/tool"
 )
 
 func init() {
