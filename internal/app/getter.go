3a1f14d06b63bfb7ed200f0ea6bd8016870cf826
xiaofei-ya <75562054+xiaofei-ya@users.noreply.github.com>
2020-12-22 18:50:21 +0800

    Update getter.go

--- internal/app/getter.go
+++ internal/app/getter.go
@@ -2,14 +2,14 @@ package app
 
 import (
 	"errors"
-	"github.com/Sansui233/proxypool/log"
+	"github.com/xiaofei-ya/proxypool/log"
 
-	"github.com/Sansui233/proxypool/internal/cache"
+	"github.com/xiaofei-ya/proxypool/internal/cache"
 
 	"github.com/ghodss/yaml"
 
-	"github.com/Sansui233/proxypool/config"
-	"github.com/Sansui233/proxypool/pkg/getter"
+	"github.com/xiaofei-ya/proxypool/config"
+	"github.com/xiaofei-ya/proxypool/pkg/getter"
 )
 
 var Getters = make([]getter.Getter, 0)
