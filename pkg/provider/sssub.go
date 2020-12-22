69692b7eb108b92e2a6aeea45ef554bf251c4ba3
xiaofei-ya <75562054+xiaofei-ya@users.noreply.github.com>
2020-12-22 19:05:52 +0800

    Update sssub.go

--- pkg/provider/sssub.go
+++ pkg/provider/sssub.go
@@ -2,11 +2,11 @@ package provider
 
 import (
 	"encoding/json"
-	"github.com/Sansui233/proxypool/pkg/tool"
+	"github.com/xiaofei-ya/proxypool/pkg/tool"
 	"strconv"
 	"strings"
 
-	"github.com/Sansui233/proxypool/pkg/proxy"
+	"github.com/xiaofei-ya/proxypool/pkg/proxy"
 )
 
 type SSSub struct {
